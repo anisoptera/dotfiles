787a98f2f8e3a4826c9621f7327303e0dfdd66a9
Isis Lynn McCafferty <isis@lotuswind.net>
2021-06-10 22:35:51 -0700

    use the right miniconda

--- conda/install.sh
+++ conda/install.sh
@@ -4,7 +4,12 @@ which conda > /dev/null
 if [ $? -ne 0 ]
 then
 	tmpfile=$(mktemp)
-	curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > $tmpfile
-	yes | bash $tmpfile
+	if [[ $(uname) = 'Darwin' ]]
+	then
+		curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > $tmpfile
+	else
+		curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > $tmpfile
+	fi
+	bash $tmpfile
 fi
 conda config --set changeps1 False
