6259e1094bbdf0577481c0dd7e68df24a6e729ce
Isis Anisoptera <isis@lotuswind.net>
2022-08-23 15:19:25 -0700

    Now with actual correct repo

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-[ -d ~/.emacs.d ] || (git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d ; ~/.emacs.d/bin/doom install)
+[ -d ~/.emacs.d ] || (git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d ; ~/.emacs.d/bin/doom install)
+# [ -d ~/.spacemacs.d ] || 
